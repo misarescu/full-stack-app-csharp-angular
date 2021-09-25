5daed848dfdd1c536e5dc409cd562d93b6a961a6
= <redshadowfx60@gmail.com>
2021-09-26 00:39:42 +0300

    Implemented the front-end that consumes the json response from the back-end

--- HealthCheck/Startup.cs
+++ HealthCheck/Startup.cs
@@ -44,7 +44,7 @@ namespace HealthCheck
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHosts();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
