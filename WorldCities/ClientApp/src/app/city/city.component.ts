207fa03ae2ab2ee71e57ba0a6d13c64a20218f47
= <redshadowfx60@gmail.com>
2021-11-03 00:38:00 +0200

    modified the /cities endpoint to paginate on the server side. also made the paginator in angular smarter(there is a bug where on nex/previous page it resets to the first default value, this should be solved)

--- WorldCities/ClientApp/src/app/city/city.component.ts
+++ WorldCities/ClientApp/src/app/city/city.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 import { City } from './city';
 
@@ -12,6 +12,8 @@ import { City } from './city';
   styleUrls: ['./city.component.css']
 })
 export class CityComponent {
+  public pageIndex: number;
+  public pageSize: number;
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
 
   // this is the way that connects the paginator with the table
@@ -21,20 +23,36 @@ export class CityComponent {
   // and don't work together
   //public cities: City[];
 
-  @ViewChild(MatPaginator, {static:false}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   
   constructor(
     private http: HttpClient,
-    @Inject('BASE_URL') private baseUrl:string) { }
+    @Inject('BASE_URL') private baseUrl: string) { }
 
 
   ngOnInit() {
-    this.http.get<City[]>(this.baseUrl + 'api/Cities')
-      .subscribe(result => {
-        this.cities = new MatTableDataSource(result);
-        this.cities.paginator = this.paginator;
-      }, error => console.log(error));
-    
+    var pageEvent = new PageEvent();
+    pageEvent.pageIndex = 0;
+    pageEvent.pageSize = 10;
+    this.pageIndex = 0;
+    this.pageSize = 10;
+    this.getData(pageEvent);
   }
- 
+
+  getData(event: PageEvent) {
+    var url = this.baseUrl + 'api/Cities';
+    var params = new HttpParams()
+      .set("pageIndex", event.pageIndex.toString())
+      .set("pageSize", event.pageSize.toString());
+    console.log(params);
+    this.http.get<any>(url, { params })
+      .subscribe(result => {
+        this.paginator.length = result.totalCount;
+        this.paginator.pageIndex = result.pageIndex;
+        this.paginator.pageSize = result.pageSize;
+        this.pageIndex = result.pageIndex;
+        this.pageSize = result.pageSize;
+        this.cities = new MatTableDataSource<City>(result.data);
+      }, error => console.error(error));
+  }
 }
